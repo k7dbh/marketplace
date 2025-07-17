42f45dd252e3deab5e51ec9f2f8bcbf09a542c21
k7dbh <203570242+k7dbh@users.noreply.github.com>
2025-07-17 16:51:11 +0300

    created file upload

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const Listing = require('../models/listing')// step 8
 const isSignedIn = require('../middleware/is-signed-in')
+const upload = require('../config/multer')// step 9
 
 // view New LISTING FORM
 router.get("/new", (req, res) => {
@@ -11,9 +12,13 @@ router.get("/new", (req, res) => {
 
 // POST FORM DATA TO THE DATABASE
 // note to come back to isSignedIn
-router.post('/', async (req, res) => {
+router.post('/', isSignedIn, upload.single('image'), async (req, res) => {
     try {
         req.body.seller = req.session.user._id
+        req.body.image = {
+            url: req.file.path,
+            cloudinary_id: req.file.filename 
+        }
         await Listing.create(req.body)
         res.redirect('/listings') // step 11
     } catch (error) {
@@ -29,24 +34,62 @@ router.get('/', async (req, res) => {
     res.render('listings/index.ejs', { foundListings: foundListings })// step 12
 })
 
-// VIEW A SINGLE LISTING (SHOW PAGE)
+// VIEW A SINGLE LISTING (SHOW PAGE) step 16
 router.get('/:listingId', async (req, res) => {
-    const foundListing = await Listing.findById(req.params.listingId).populate('seller') // step 16
-    console.log(foundListing)
-    res.render('listings/show.ejs', {foundListing: foundListing})
+    try {
+        const foundListing = await Listing.findById(req.params.listingId).populate('seller').populate('comments.author')
+        console.log(foundListing)
+        res.render('listings/show.ejs', { foundListing: foundListing })
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
 })
 
+
 // DELETE LISTING FROM DATABASE
-router.delete('/:listingId', async (req, res) => {
-    // find the listing 
+router.delete('/:listingId', isSignedIn, async (req, res) => {
+    // find the listing
     const foundListing = await Listing.findById(req.params.listingId).populate('seller')
     // check if the logged in user owns the listing
-    if(foundListing.seller._id.equals(req.session.user._id)){
+    if (foundListing.seller._id.equals(req.session.user._id)) {
         // delete the listing and redirect
         await foundListing.deleteOne()
-        res.redirect('/listings')
+        return res.redirect('/listings')
     }
     return res.send('Not authorized')
+})
+
+// RENDER THE EDIT FROM VIEW
+router.get('/:listingId/edit', async (req, res) => {
+    const foundListing = await Listing.findById(req.params.listingId).populate('seller')
+
+    if (foundListing.seller._id.equals(req.session.user._id)) {
+        return res.render('listings/edit.ejs', { foundListing: foundListing} )
+    } 
+        return res.send('Not authorized')
+   
+
+})
+
+router.put('/:listingId', isSignedIn, async (req, res) => {
+    const foundListing = await Listing.findById(req.params.listingId).populate('seller')
+    if (foundListing.seller._id.equals(req.session.user._id)) {
+        await Listing.findByIdAndUpdate(req.params.listingId, req.body, { new: true })
+        return res.redirect(`/listings/${req.params.listingId}`)
+    } 
+        return res.send('Not authorized')
+   
+})
+
+// POST COMMENTS TO THE DATABASE
+router.post('/:listingId/comments', isSignedIn, async (req, res) => {
+    const foundListing = await Listing.findById(req.params.listingId)
+    req.body.author = req.session.user._id
+    console.log(foundListing)
+    foundListing.comments.push(req.body)
+    await foundListing.save()
+    res.redirect(`/listings/${req.params.listingId}`)
 })
 
 module.exports = router
--- models/listing.js
+++ models/listing.js
@@ -1,17 +1,29 @@
-// Step 1
+//step 1
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
+const commentSchema = new mongoose.Schema({
+    content: String,
+    author: { 
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, { timestamps: true })
 
 const listingSchema = new mongoose.Schema({
     title: String,
     description: String,
     price: Number, 
-    image: String,
-    seller: {
-        type: mongoose.Schema.Types.ObjectId,
+    image: {
+        url: { type: String, required: true },
+        cloudinary_id: { type: String, required: true}
+        
+    },
+    seller: { 
+        type: Schema.Types.ObjectId,
         ref: 'User'
-    }
-}, { timestamps: true})
+    },
+    comments: [commentSchema]
+}, { timestamps: true })
 
 module.exports = mongoose.model('Listing', listingSchema)
-
